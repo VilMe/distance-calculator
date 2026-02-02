ea24483754339f7d47f1451ea80883ff6e47dd74
VilMe <vilma.medrano00@gmail.com>
2026-02-01 22:53:30 -0600

    define get_coordinates function

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ class Coordinates:
         return self.latitude, self.longitude
     
 
-    
-    
+def get_coordinates(address: str) -> Coordinates | None:
+    geolocator = Nominatim(user.agent='distance_calculator')
+    location = geolocator.geocode(address)
+
+    if location:
+        return Coordinates(latitude=location.latitude, longitude=location.longitude)
