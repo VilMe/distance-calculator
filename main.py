a0a3afac49a1e94729a124085be6ef37d5cf31c2
VilMe <vilma.medrano00@gmail.com>
2026-02-01 22:48:04 -0600

    define Coordinates dataclass and function to return coordinates, latitude and longitude

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
 
+
+@dataclass
+class Coordinates:
+    latitude: float
+    longitude: float
+
+
+    def coordinates(self):
+        return self.latitude, self.longitude
+    
+
+    
+    
